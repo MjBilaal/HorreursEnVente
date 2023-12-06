1fb6d0ea128474bea80fd1c6719a3499e90f5fac
Jeryco23 <148986455+Jeryco23@users.noreply.github.com>
2023-12-06 15:19:13 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -22,6 +22,27 @@ if (!$resultat) {
     die("La requête a échoué : " . $connexion->error);
 }
 
+// Vérifie si l'ID du produit est passé en paramètre
+if (isset($_GET['produit_id'])) {
+    $produit_id = $_GET['produit_id'];
+
+  // Requête SQL pour récupérer les détails du produit
+  $requete_produit = "SELECT * FROM produits WHERE id = $produit_id";
+  $resultat_produit = $connexion->query($requete_produit);
+
+  // Ajoute le produit au panier
+  $requete_panier = "INSERT INTO panier (produit_id, produit_nom, prix) VALUES ('{$produit['id']}', '{$produit['nom']}', '{$produit['prix']}')";
+  $resultat_panier = $connexion->query($requete_panier);
+  
+  if ($resultat_panier) {
+              echo "Le produit a été ajouté au panier avec succès!";
+          } else {
+              echo "L'ajout au panier a échoué : " . $connexion->error;
+          }
+      } else {
+          echo "Produit non trouvé.";
+      }
+  }
   // Fermeture de la connexion à la base de données
   $connexion->close();
 ?>
